0a0feb110ae07a4b7dc7b1978bd8e2239656696f
Oksana(KsyFate) <oprokopieva382@gmail.com>
2023-11-29 13:16:42 -0600

    create skeleton for About me page, still need content

---
+++ src/components/About.tsx
@@ -0,0 +1,37 @@
+import { Box } from "@mui/material";
+import video from "../assets/video/aboutBackground.mp4";
+
+export const About = () => {
+  return (
+    <>
+      <Box>
+        <video
+          autoPlay
+          loop
+          muted
+          src={video}
+          style={{
+            width: "100%",
+            height: "100vh",
+            objectFit: "cover",
+          }}
+        />
+        <Box
+          sx={{
+            position: "absolute",
+            width: "100%",
+            height: "100%",
+            top: 0,
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            color: "white",
+          }}
+        >
+          <h1>Yo</h1>
+        </Box>
+      </Box>
+    </>
+  );
+};
