177c688a1dbc1d625581659ed4c1e8079f318dba
Oksana(KsyFate) <oprokopieva382@gmail.com>
2023-12-01 15:55:42 -0600

    add Error page content

--- src/components/pages/Error.tsx
+++ src/components/pages/Error.tsx
@@ -1,4 +1,36 @@
+import { Box, Typography } from "@mui/material";
+import error from "../../assets/video/error.mp4";
+
 export const Error = () => {
-    return (
-        <div>Error</div>
-    )}
+  return (
+    <Box>
+      <video
+        autoPlay
+        loop
+        muted
+        src={error}
+        style={{
+          width: "100%",
+          height: "100vh",
+          objectFit: "cover",
+          display: "block",
+        }}
+      />
+      <Box
+        sx={{
+          position: "absolute",
+          width: "100%",
+          height: "100%",
+          top: 0,
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          color: "white",
+        }}
+      >
+        <Typography>Page is not found</Typography>
+      </Box>
+    </Box>
+  );
+};
