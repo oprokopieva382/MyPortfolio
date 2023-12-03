615dc699c6cebdddcfa064415c241edb14db7cf0
Oksana(KsyFate) <oprokopieva382@gmail.com>
2023-12-03 15:04:48 -0600

    move out style from Project.tsx to separate style file

--- src/components/pages/Projects.tsx
+++ src/components/pages/Projects.tsx
@@ -1,18 +1,11 @@
 import { ProjectCard } from "../ProjectCard";
-import { Box, Grid } from "@mui/material";
+import { Grid } from "@mui/material";
 import projects from "../../data/projectData.tsx";
+import { ProjectsContainer } from "../../style/ProjectStyle.tsx";
 
 export const Projects = () => {
   return (
-    <Box
-      sx={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        minHeight: "100vh",
-        background: "linear-gradient(to right, #F7F8F8, #ACBB78)",
-      }}
-    >
+    <ProjectsContainer>
       <Grid container marginTop={10} padding="10px">
         {projects.map((project) => (
           <Grid key={project.id} item xs={12} sm={6} md={4} lg={3} padding={2}>
@@ -25,6 +18,6 @@ export const Projects = () => {
           </Grid>
         ))}
       </Grid>
-    </Box>
+    </ProjectsContainer>
   );
 };
--- src/style/ProjectStyle.tsx
+++ src/style/ProjectStyle.tsx
@@ -1,1 +1,10 @@
+import { Box } from "@mui/material";
+import { styled } from "@mui/system";
 
+export const ProjectsContainer = styled(Box)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+  background: linear-gradient(to right, #f7f8f8, #acbb78);
+`;
