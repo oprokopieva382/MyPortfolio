49df30dc6b5ec201298546e4f935ebe0300204db
Oksana(KsyFate) <oprokopieva382@gmail.com>
2023-12-03 15:59:12 -0600

    code improvement for Error.tsx

---
+++ src/style/UniversalStyle.tsx
@@ -0,0 +1,14 @@
+import { Box } from "@mui/material";
+import { styled } from "@mui/system";
+
+export const UniversalBoxContainer = styled(Box)`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  color: white;
+`;
