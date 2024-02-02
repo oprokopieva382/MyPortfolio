37724a5a9146d86e85aa2d8a8464553b89f78ba0
Oksana(KsyFate) <oprokopieva382@gmail.com>
2024-02-02 12:18:10 -0600

    finish strucrures of Greeting page

--- src/components/pages/Greeting.tsx
+++ src/components/pages/Greeting.tsx
@@ -1,8 +1,57 @@
-import { StyledContainer } from "../../style/GreetingStyle";
+import {
+  StyledBoxButtons,
+  StyledBoxSkills,
+  StyledButton,
+  StyledContainer,
+  StyledTypography,
+} from "../../style/GreetingStyle";
+import { PiLinkedinLogoLight } from "react-icons/pi";
+import { PiFolderUserThin } from "react-icons/pi";
 
+const skills = [
+  "React.js",
+  "TypeScript",
+  "JavaScript",
+  "Node.js",
+  "REST API's",
+  "Axios",
+  "Express.js",
+  "Redux",
+  "GraphQL",
+  "JWT",
+  "Material UI",
+  "Bootstrap",
+  "styled-components",
+  "Figma",
+  "MongoDB",
+  "Mongoose",
+  "Jest",
+  "RTL",
+  "Git",
+  "CSS3",
+  "HTML5",
+];
 
 export const Greeting = () => {
-    return (
-        <StyledContainer>Here</StyledContainer>
-    );
-}
+  return (
+    <>
+      <StyledContainer>
+        <StyledBoxSkills>
+          {skills.map((skill, i) => {
+            return <StyledTypography key={i}>{skill}</StyledTypography>;
+          })}
+        </StyledBoxSkills>
+        <StyledBoxButtons>
+          <StyledButton>
+            <PiLinkedinLogoLight size="1.5em" />
+            LinkedIn
+          </StyledButton>
+          <StyledButton>
+            <PiFolderUserThin size="1.5em" />
+            Portfolio
+          </StyledButton>
+        </StyledBoxButtons>
+      </StyledContainer>
+    </>
+  );
+};
